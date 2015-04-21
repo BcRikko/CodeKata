4766a57d0dc80bab3811eb1e722f88a2667ca32b
bc_rikko <s.shin1106@gmail.com>
2015-04-21 15:11:54 +0900

    要素3つの場合

--- CodeKata/test/Kata02_KarateChopSpec.ts
+++ CodeKata/test/Kata02_KarateChopSpec.ts
@@ -24,4 +24,19 @@ describe('CodeKata02: KarateChop',() => {
         var result = this.karate.chop(1, [1, 3, 5]);
         expect(result).toBe(0);
     });
+
+    it('要素3つで、2つ目で見つかった場合、1が返ってくる',() => {
+        var result = this.karate.chop(3, [1, 3, 5]);
+        expect(result).toBe(1);
+    });
+
+    it('要素3つで、3つ目で見つかった場合、2が返ってくる',() => {
+        var result = this.karate.chop(5, [1, 3, 5]);
+        expect(result).toBe(2);
+    });
+
+    it('要素3つで、見つからない場合、-1が返ってくる',() => {
+        var result = this.karate.chop(2, [1, 3, 5]);
+        expect(result).toBe(-1);
+    });
 });
